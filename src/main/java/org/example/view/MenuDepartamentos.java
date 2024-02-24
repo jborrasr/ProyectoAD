6fef9446070420339e8b2e44b9ebe9ed2ab2d34d
Jordi <jordiborrasroca@gmail.com>
2024-02-24 17:54:03 +0100

    commit 2

---
+++ src/main/java/org/example/view/MenuDepartamentos.java
@@ -0,0 +1,20 @@
+package org.example.view;
+
+import javax.swing.*;
+
+public class MenuDepartamentos extends JDialog {
+    private JPanel contentPane;
+    private JButton btnInsertarDepartamentos;
+    private JButton btnModificarDepartamentos;
+    private JButton btnEliminarDepartamentos;
+    private JButton btnVolver;
+
+
+
+public MenuDepartamentos() {
+        setContentPane(contentPane);
+        setModal(true);
+    }
+
+
+}
